9ea1214f047e6d0c3f534d55054eb09a18e299b8
JaViKo500 <javikogutierrez64@gmail.com>
2024-02-28 00:04:46 -0500

    first commit

---
+++ jkTitle.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   * Plugin Name: Jk Title
+   * Plugin URI: https://github.com/
+   * Description: test plugin
+   * Version: 1.0
+   * Author: Javiko500
+   * Author URI: https://github.com/JaviKo500
+   * License: GPL2
+   */
+
+?>
