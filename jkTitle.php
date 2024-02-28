2913f582a29f24909e620376201001bbc53459fb
JaViKo500 <javikogutierrez64@gmail.com>
2024-02-28 00:27:23 -0500

    add hooks

--- jkTitle.php
+++ jkTitle.php
@@ -8,5 +8,20 @@
    * Author URI: https://github.com/JaviKo500
    * License: GPL2
    */
+   add_filter('the_title', 'updateTitles');
+   // add_filter('the_content', 'updateContentText');
+   add_filter('list_cats', 'updateCategoriesText');
 
+   // modify title
+   function updateTitles ($text) {
+      return 'Filter text example ---> '.$text;
+   }
+
+   function updateContentText( $text ) {
+      return strtoupper( $text );
+   }
+
+   function updateCategoriesText( $text ) {
+      return strtolower( $text );
+   }
 ?>
